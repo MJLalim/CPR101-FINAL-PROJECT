3e4d0c1a84e2a0ca4efe3de642bae33f10b62a1c
parthmern <parthmern@gmail.com>
2024-04-02 12:42:03 -0400

    V2: changes in fundamentals.c

--- fundamentals.c
+++ fundamentals.c
@@ -11,32 +11,21 @@
 
 
 void fundamentals(void) {
-// V1
+// V2
 
-    printf("*** Start of Indexing Strings Demo ***\n");
-    char buffer1[BUFFER_SIZE];
-    char numInput[NUM_INPUT_SIZE];
-    size_t position; 
-
+    printf("*** Start of Measuring Strings Demo ***\n");
+    char buffer2[BUFFER_SIZE];
     do {
-        printf("Type not empty string (q - to quit):\n");
-        fgets(buffer1, BUFFER_SIZE, stdin);
-        buffer1[strlen(buffer1) - 1] = '\0';
+        printf("Type a string (q - to quit):\n");
+        fgets(buffer2, BUFFER_SIZE, stdin);
+        buffer2[strlen(buffer2) - 1] = '\0'; 
 
-        if (strcmp(buffer1, "q") != 0) {
-            printf("Type the character position within the string:\n");
-            fgets(numInput, NUM_INPUT_SIZE, stdin);
-            numInput[strlen(numInput) - 1] = '\0';
-            position = atoi(numInput);
-
-            if (position >= strlen(buffer1)) {
-                position = strlen(buffer1) - 1;
-                printf("Too big... Position reduced to max. available\n");
-            }
-
-            printf("The character found at %d position is \'%c\'\n", (int)position, buffer1[position]);
+        if (strcmp(buffer2, "q") != 0) {
+            printf("The length of '%s' is %d characters\n", buffer2, (int)strlen(buffer2)); 
         }
-    } while (strcmp(buffer1, "q") != 0);
+    } while (strcmp(buffer2, "q") != 0); 
 
-    printf("*** End of Indexing Strings Demo ***\n\n");
+    printf("*** End of Measuring Strings Demo ***\n\n");
+
+    return 0;
 }
